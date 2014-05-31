63eb8fd998beb1196373d134b45adf7a2173b448
nahonyae <nahon.yael@gmail.com>
2014-05-31 16:28:10 +0300

    d

---
+++ root/sadnaServer/src/Server/Forum/Logger.java
@@ -0,0 +1,15 @@
+package Server.Forum;
+import Server.Users.*;
+import Server.Posts.*;
+public class Logger {
+
+	public Logger(){
+		//TODO
+	}
+	
+	public void getFormat(String msg, String level){
+		//TODO
+	}
+	
+	
+}
